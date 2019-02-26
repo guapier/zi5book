5492989f7141e0d4dc21334612e8748aff20d3a4
guapier <451875565@qq.com>
2019-02-26 12:27:37 +0800

    fix the rule bugs

--- zi5book/spiders/zi5book_spider.py
+++ zi5book/spiders/zi5book_spider.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from scrapy import Request
 import scrapy
-
+import re
+from scrapy import Request
 from zi5book.items import Zi5BookItem
 
 
@@ -38,20 +38,32 @@ class Zi5bookSpiderSpider(scrapy.Spider):
         item = Zi5BookItem()
         item['name'] = response.css('.h1-wrapper > h1:nth-child(1)::text').extract_first()
         item['author'] = response.css('.post-meta-top > div:nth-child(2) > a:nth-child(1)::text').extract_first()
-        item['time'] = response.css('.post-meta-top > div:nth-child(2)::text').extract_first()
+        item['time'] = response.css('.post-meta-top > div:nth-child(2)::text').extract_first().replace('\xa0|\xa0 ', '')
         item['publisher'] = response.css('.post-meta-top > div:nth-child(2) > a:nth-child(2)::text').extract_first()
         item['comment'] = response.css('.post-meta-top > div:nth-child(1) > a:nth-child(1)::text').extract_first()
-        item['view'] = response.css('.post-meta-top > div:nth-child(1)::text').extract_first()
-        item['ISBN'] = response.css(
-            '#post-936 > div:nth-child(3) > div:nth-child(1) > a:nth-child(1)::text').extract_first()
+        item['view'] = response.css('.post-meta-top > div:nth-child(1)::text').extract_first().replace('|','').replace('views','').strip()
+        # item['ISBN'] = response.css(
+        #     '#post-936 > div:nth-child(3) > div:nth-child(1) > a:nth-child(1)::text').extract_first()
+        ISBN = re.findall("title='跳转至豆瓣'>(.*?)</a>", response.text)
+        if ISBN:
+            item['ISBN'] = ISBN[0]
+        else:
+            item['ISBN'] = ''
+
         item['rates'] = response.css('.rateNum::text').extract_first()
-        item['updated'] = response.css('#post-936 > div:nth-child(3)::text').extract_first()
-        item['desc'] = response.css('.post-content > p:nth-child(2)::text').extract_first()
+        # item['updated'] = response.css('#post-936 > div:nth-child(3)::text').extract_first()
+        updated = re.findall('更新时间：(.*?)</div>', response.text)
+        if updated:
+            item['updated'] = updated[0]
+        else:
+            item['updated'] = ''
+
+        item['desc'] = ''.join(response.xpath('//p[@class="description"]/text()').extract())
         item['image_urls'] = [response.urljoin(image_url) for image_url in
                               response.css('.post-content > img:nth-child(1)::attr(src)').extract()]
         item['up'] = response.css('.thumbs-rating-up::text').extract_first()
         item['down'] = response.css('.thumbs-rating-down::text').extract_first()
-        item['tags'] = response.css('.post-meta-category-tag::text').extract_first()
+        item['tags'] = ''.join(response.css('.post-meta-category-tag a::text').extract())
         item['file_urls'] = response.css('a.download-link::attr(href)').extract()
 
         yield item
